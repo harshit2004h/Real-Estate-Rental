4a2ad2c7d91c4919c0720428e3da04779842a8fe
harshit2004h <harshitsrivastava2004h@gmail.com>
2025-05-13 22:30:32 +0530

    landing page done backend setup

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,3 +1,7 @@
+import CallToActionSection from "@/components/landing/CallToActionSection";
+import DiscoverSection from "@/components/landing/DiscoverSection";
+import FeaturesSection from "@/components/landing/FeaturesSection";
+import FooterSection from "@/components/landing/FooterSection";
 import HeroSection from "@/components/landing/HeroSection";
 import React from "react";
 
@@ -5,6 +9,10 @@ const Landing = () => {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
     </div>
   );
 };
